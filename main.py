b7ccba839b18570ee57cdcbf356bf9122b3c6bf5
akbay74 <asv1974@list.ru>
2024-08-03 08:16:25 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+home_work = 12
+hour_work = 1.5
+name = 'Python'
+time_work = hour_work / home_work
+print('Курс: ',name,', всего задач: ',home_work,', затрачено часов: ',hour_work,
+      ', среднее время выполнения ',time_work,' часа.')
